c7f3a62c505b46cb81b4b8cc809d05b0356eec52
kirayadav123 <115138483+kirayadav123@users.noreply.github.com>
2023-02-05 00:05:16 +0530

    end section of 3

---
+++ NZWalks/NZWalks.API/Models/DTO/Region.cs
@@ -0,0 +1,14 @@
+﻿namespace NZWalks.API.Models.DTO
+{
+    public class Region
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public double Area { get; set; }
+        public Double Lat { get; set; }
+        public Double Long { get; set; }
+        public double Population { get; set; }
+
+    }
+}
