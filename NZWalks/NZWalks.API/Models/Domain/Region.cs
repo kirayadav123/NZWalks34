55beaa92fe22755d402ca5557e4928111436cb17
kirayadav123 <115138483+kirayadav123@users.noreply.github.com>
2023-02-04 18:22:39 +0530

    End of section 2

---
+++ NZWalks/NZWalks.API/Models/Domain/Region.cs
@@ -0,0 +1,17 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Region
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public double Area  { get; set; }
+        public Double Lat { get; set; }
+        public Double Long { get; set; }
+        public double Population { get; set; }
+
+       //Navigation property
+       public IEnumerable<Walk> Walks  { get; set;}
+
+    }
+}
